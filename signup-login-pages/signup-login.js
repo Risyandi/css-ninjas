3b74d9152f12fb018bbaf5d88541eb28da1dc96a
risyandi <risyandi.94@gmail.com>
2019-03-24 09:18:13 +0700

    modified js

--- signup-login-pages/signup-login.js
+++ signup-login-pages/signup-login.js
@@ -3,13 +3,9 @@ const buttonSignIn = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 buttonSignUp.addEventListener('click', () => {
-    console.log("click button");
-    container.classList.remove('left-panel-active');
     container.classList.add('right-panel-active');
 });
 
 buttonSignIn.addEventListener('click', () => {
-    console.log("click button");
     container.classList.remove('right-panel-active');
-    container.classList.add('left-panel-active');
 });
