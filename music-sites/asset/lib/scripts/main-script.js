9e6b7dc35074e57ed1011ad04bd46b17bdd25757
risyandi <risyandi.94@gmail.com>
2019-11-13 15:40:59 +0700

    update animate on gadget

--- music-sites/asset/lib/scripts/main-script.js
+++ music-sites/asset/lib/scripts/main-script.js
@@ -1,3 +1,4 @@
+let elemGadget = document.getElementById("gadget");
 new WOW().init();
 
 function wowAnimate() {
@@ -10,3 +11,34 @@ function wowAnimate() {
     }
     return wow;
 }
+
+function addingClass(paramID, paramClass) {
+    let elementID = document.getElementById(paramID);
+    if (paramID != null) {
+        elementID.classList.add(paramClass);
+    }
+}
+
+function removeClass(paramID, paramClass) {
+    let elementID = document.getElementById(paramID);
+    if (paramID != null) {
+        elementID.classList.remove(paramClass);
+    }
+}
+
+function changesAnimationName(paramID, animationName) {
+    let elementID = document.getElementById(paramID);
+    if (paramID != null) {
+        elementID.style.animationName = animationName;
+    }
+}
+
+elemGadget.addEventListener("click", function () {
+    changesAnimationName("gadget", "");
+    removeClass("gadget", "rotateInDownLeft");
+    if (elemGadget.classList.contains("wobble")) {
+        removeClass("gadget", "wobble");
+    } else {
+        addingClass("gadget", "wobble");
+    }
+});
